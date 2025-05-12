7100566cd2991818e5dfe6d80c520c18414cafe0
Amanda Negri <a824132077@gmail.com>
2025-05-12 20:08:04 -0300

    feat(back): incluindo o .env para a chave de api

--- back/index.js
+++ back/index.js
@@ -1,9 +1,11 @@
+require ('dotenv').config()
 // endpoint: receber requisições HTTP
 const express = require ('express')
 const {GoogleGenAI} = require ('@google/genai') // import google gemini
 const app = express ()
 //criar função modular
 app.use (express.json())
+
 
 // endpoint: tem um  método HTTP, padrão de acesso, funcionalidade
 //GET (obter), POST(postar/lançar), PUT (atualizar), DELETE (deletar)
@@ -12,7 +14,7 @@ app.use (express.json())
 //terceiro funcionalidade () => {} 
 // API coleção de endpoint's
 app.post('/pergunte-ao-gemini', async (req, res) => {
-    const ai = new GoogleGenAI({ apiKey: "" });
+    const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
     const prompt = req.body.prompt //recebe requsição
     const response = await ai.models.generateContent({
         model: "gemini-2.0-flash",
