f17ecb41e45e87a6c1150cd5915f0aaa1a35fc63
Amanda Negri <a824132077@gmail.com>
2025-05-20 21:51:57 -0300

    feat(ap08-hooks):cria componente Busca e Accordion
    - faz um efeito sanfona
    - inclui input com icone de busca

---
+++ ap08-hooks/src/components/Busca.jsx
@@ -0,0 +1,13 @@
+import {InputText} from 'primereact/inputtext'
+
+const Busca = () => {
+    return (
+        <div>
+            <span className='p-input-icon-left'>
+                <i className="pi pi-search"></i>
+                <InputText/>
+            </span>
+        </div>
+    )
+}
+export default Busca
--- ap08-hooks/src/main.jsx
+++ ap08-hooks/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'primeflex/primeflex.min.css'
-import 'primeicons/primeicons.min.css'
+import 'primeicons/primeicons.css'
 import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/tailwind-light/theme.css'
 
