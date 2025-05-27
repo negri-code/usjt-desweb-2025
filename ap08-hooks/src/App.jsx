16881b18fe37eaab73033e410c76d98092c35c85
Amanda Negri <a824132077@gmail.com>
2025-05-27 12:57:45 -0300

    feat(ap08-hooks):encerra secao 2.17
    - realiza a configuração html perigoso
    - inclui uma api da wikipedia para retornar resultados relacionados a pesquisa feita
    - utiliza useState e useEffect
    - faz uma requisiçãp http axios get
    - utiliza cors

--- ap08-hooks/src/App.jsx
+++ ap08-hooks/src/App.jsx
@@ -1,21 +1,21 @@
-import Accordion from "./components/Accordion"
+// import Accordion from "./components/Accordion"
 import Busca from "./components/Busca"
 
-const itens = [
-  {
-    titulo:'Java',
-    conteudo: 'Linguagem compilada e interpretada'
-  },
-  {
-    titulo: 'Python',
-    conteudo: 'Linguagem interpretada e dinamicamente tipada'
-  },
-  {
-    titulo: 'Javascript',
-    conteudo: 'Interpretada. Executa do lado cliente e do lado servidor também'
-  }
+  // const itens = [
+  //   {
+  //     titulo:'Java',
+  //     conteudo: 'Linguagem compilada e interpretada'
+  //   },
+  //   {
+  //     titulo: 'Python',
+  //     conteudo: 'Linguagem interpretada e dinamicamente tipada'
+  //   },
+  //   {
+  //     titulo: 'Javascript',
+  //     conteudo: 'Interpretada. Executa do lado cliente e do lado servidor também'
+  //   }
 
-]
+  // ]
 const App = () => {
   const expressaoJSX = <Busca/>
   return(
